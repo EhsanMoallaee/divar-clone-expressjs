c7caa22334144f0d273b236d63c8c7656ef4dd83
Ehsan Moallaee <ehsanm78@gmail.com>
2023-12-30 00:56:09 +0330

    Main: Cookie-parser and path packages installed and serving static folder done

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/static', express.static(path.join(__dirname, '..', 'static')));
 
 export default app;
