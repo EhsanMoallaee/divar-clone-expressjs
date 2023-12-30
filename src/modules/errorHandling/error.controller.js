f67f85fa6bfda3960b6df221628289d2304175e5
Ehsan Moallaee <ehsanm78@gmail.com>
2023-12-30 03:36:43 +0330

    Main: Error handling module to catch async errors created

---
+++ src/modules/errorHandling/error.controller.js
@@ -0,0 +1,20 @@
+const sendErrorDevelopmentMode = (err, res) => {
+	const statusCode = err.statusCode || 500;
+	return res.status(statusCode).json({
+		message: err.message,
+		stack: err.stack,
+	});
+};
+
+const sendErrorProductionMode = (err, res) => {
+	const statusCode = err.statusCode || 500;
+	return res.status(statusCode).json({
+		message: err.message,
+	});
+};
+
+// eslint-disable-next-line no-unused-vars
+export default (err, req, res, next) => {
+	if (process.env.NODE_ENV === 'development') sendErrorDevelopmentMode(err, res);
+	else sendErrorProductionMode(err, res);
+};
