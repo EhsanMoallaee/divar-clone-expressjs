5de20e59b449d4f352b8dbc14f9ba574a48e64b1
Ehsan Moallaee <ehsanm78@gmail.com>
2024-01-03 11:05:49 +0330

    Main: Swagger config file path corrected

---
+++ src/swagger/swagger.config.js
@@ -0,0 +1,26 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+function swaggerConfig(app) {
+	const swaggerDocument = swaggerJSDoc({
+		swaggerDefinition: {
+			openapi: '3.1.0',
+			info: {
+				title: 'Divar Clone',
+				description: 'Expressjs & Mongodb',
+				version: '1.0.0',
+			},
+			servers: [
+				{
+					url: `http://localhost:3000`,
+				},
+			],
+		},
+		apis: ['./src/modules/**/*.swagger.js', './src/modules/**/*.schemas.js'],
+	});
+
+	const swagger = swaggerUi.setup(swaggerDocument, {});
+	app.use('/swagger', swaggerUi.serve, swagger);
+}
+
+export default swaggerConfig;
