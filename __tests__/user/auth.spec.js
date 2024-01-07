f297d234ab3a996bd396bfd8bb3041c3190f05ff
Ehsan Moallaee <ehsanm78@gmail.com>
2024-01-07 13:58:14 +0330

    Main: Add some tests to profile.spec.js and improved profile and auth tests

--- __tests__/user/auth.spec.js
+++ __tests__/user/auth.spec.js
@@ -8,9 +8,6 @@ import authSuccessMessages from '../../src/modules/user/auth/messages/auth.succe
 import authErrorMessages from '../../src/modules/user/auth/messages/auth.errorMessages.js';
 import redisSingletonInstance from '../../src/modules/redisClient/redis.client.js';
 
-// import dotenv from 'dotenv';
-// dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-
 beforeAll(async () => {
 	new ConnectMongodb();
 });
@@ -22,78 +19,53 @@ afterEach(async () => {
 
 const correctCredentials = config.get('userCorrectCredentials');
 const incorrectCredentials = config.get('userIncorrectCredentials');
+const registerationRequestURL = '/api/users/auth/v1/registerationRequest';
+const registerURL = '/api/users/auth/v1/register';
 
 describe('Authentication tests', () => {
 	it('Registeration request: returns 200 with correct mobile,first and last name values', async () => {
-		const response = await request(app).post('/api/users/auth/v1/registerationRequest').send(correctCredentials);
+		const response = await request(app).post(registerationRequestURL).send(correctCredentials);
 		expect(response.status).toBe(200);
 		expect(response.body.message).toBe(authSuccessMessages.OTPSentSuccessfully['message']);
 	});
 
 	it('Registeration request: returns 400 with incorrect mobile', async () => {
-		const response = await request(app).post('/api/users/auth/v1/registerationRequest').send(incorrectCredentials);
+		const response = await request(app).post(registerationRequestURL).send(incorrectCredentials);
 		expect(response.status).toBe(400);
 		expect(response.body.message).toBe(authErrorMessages.WrongMobileNumber['message']);
 	});
 
-	it('Registeration request: returns 400 if before otpCode expire send request again', async () => {
-		await request(app).post('/api/users/auth/v1/registerationRequest').send(correctCredentials);
-		const response = await request(app).post('/api/users/auth/v1/registerationRequest').send(correctCredentials);
+	it('Registeration request: returns 400 if before otpCode expiration,user sends request again', async () => {
+		await request(app).post(registerationRequestURL).send(correctCredentials);
+		const response = await request(app).post(registerationRequestURL).send(correctCredentials);
 		expect(response.status).toBe(400);
 		expect(response.body.message).toBe(authErrorMessages.SpamAttack['message']);
 	});
 
 	it('Register request: returns 200 with correct otpCode', async () => {
-		await request(app).post('/api/users/auth/v1/registerationRequest').send(correctCredentials);
+		await request(app).post(registerationRequestURL).send(correctCredentials);
 		const data = await redisSingletonInstance.getData(correctCredentials.mobile);
 		const otpCode = JSON.parse(data).otpCode;
-		const response = await request(app)
-			.post('/api/users/auth/v1/register')
-			.send({ mobile: correctCredentials.mobile, otpCode });
+		const response = await request(app).post(registerURL).send({ mobile: correctCredentials.mobile, otpCode });
 		expect(response.status).toBe(201);
 		expect(response.body.message).toBe(authSuccessMessages.RegisteredSuccessfully['message']);
 	});
 
 	it('Register request: returns 400 with incorrect otpCode', async () => {
-		await request(app).post('/api/users/auth/v1/registerationRequest').send(correctCredentials);
+		await request(app).post(registerationRequestURL).send(correctCredentials);
 		const data = await redisSingletonInstance.getData(correctCredentials.mobile);
 		const otpCode = JSON.parse(data).otpCode + 1;
-		const response = await request(app)
-			.post('/api/users/auth/v1/register')
-			.send({ mobile: correctCredentials.mobile, otpCode });
+		const response = await request(app).post(registerURL).send({ mobile: correctCredentials.mobile, otpCode });
 		expect(response.status).toBe(400);
 		expect(response.body.message).toBe(authErrorMessages.WrongOtpCode['message']);
 	});
 
 	it('Register request: returns 400 with incorrect mobile', async () => {
-		await request(app).post('/api/users/auth/v1/registerationRequest').send(correctCredentials);
+		await request(app).post(registerationRequestURL).send(correctCredentials);
 		const data = await redisSingletonInstance.getData(correctCredentials.mobile);
 		const otpCode = JSON.parse(data).otpCode;
-		const response = await request(app)
-			.post('/api/users/auth/v1/register')
-			.send({ mobile: incorrectCredentials.mobile, otpCode });
+		const response = await request(app).post(registerURL).send({ mobile: incorrectCredentials.mobile, otpCode });
 		expect(response.status).toBe(400);
 		expect(response.body.message).toBe(authErrorMessages.WrongOtpCode['message']);
 	});
 });
-
-// describe('User profile tests', () => {
-// 	it('return 400 for find one user with empty filter queries', async () => {
-// 		const response = await getUsers().query({});
-// 		expect(response.status).toBe(profileErrorMessages.EmptyFilterQuery['statusCode']);
-// 		expect(response.body.message).toBe(profileErrorMessages.EmptyFilterQuery['message']);
-// 	});
-
-// 	it('return 404 for find one user with wrong filter queries', async () => {
-// 		await addUsers(1);
-// 		const response = await getUsers().query({ firstname: 'wrong firstname' });
-// 		expect(response.status).toBe(profileErrorMessages.NotFound['statusCode']);
-// 		expect(response.body.message).toBe(profileErrorMessages.NotFound['message']);
-// 	});
-
-// 	it('return 200 for find one user with correct filter queries', async () => {
-// 		await addUsers(1);
-// 		const response = await getUsers().query({ firstname: 'user1', mobile: '10' });
-// 		expect(response.status).toBe(200);
-// 	});
-// });
--- __tests__/user/profile.spec.js
+++ __tests__/user/profile.spec.js
@@ -8,6 +8,7 @@ import { ConnectMongodb } from '../../src/dataAccessLayer/connect.database.js';
 import userRepository from '../../src/modules/user/user.repository.js';
 import UserModel from '../../src/modules/user/model/user.model.js';
 import tokenGenerator from '../../src/common/jwtToken/jwtToken.generator.js';
+import authErrorMessages from '../../src/modules/user/auth/messages/auth.errorMessages.js';
 
 beforeAll(async () => {
 	new ConnectMongodb();
@@ -18,15 +19,20 @@ afterEach(async () => {
 });
 
 const cookieSecretKey = process.env.COOKIE_SECRET_KEY;
+const findOneURL = '/api/users/profile/v1/findOne';
+const whoAmIURL = '/api/users/profile/v1/whoami';
 
 const addUsers = async (userCount) => {
+	const users = [];
 	for (let i = 0; i < userCount; i++) {
-		await userRepository.create({
+		const user = await userRepository.create({
 			firstname: `user${i}`,
 			lastname: `user${i}`,
 			mobile: `0937533111${i}`,
 		});
+		users.push(user);
 	}
+	return users;
 };
 
 const generateToken = async (payload) => {
@@ -36,44 +42,59 @@ const generateToken = async (payload) => {
 	return xAuthToken;
 };
 
-const getUsers = () => {
-	const agent = request(app).get('/api/users/profile/v1/findOne');
+const getUsers = (url) => {
+	const agent = request(app).get(url);
 	return agent;
 };
 
-const requestWithAuth = async (findOneOption = {}, query = {}) => {
+const requestWithAuth = async (findOneOption = {}, filterQuery = {}, url) => {
 	const user = await userRepository.findOne(findOneOption);
 	const xAuthToken = await generateToken({ id: user._id });
-	const response = await getUsers()
-		.query(query)
+	const response = await getUsers(url)
+		.query(filterQuery)
 		.set('Cookie', `x-auth-token=s:${sign(xAuthToken, cookieSecretKey)}`);
 	return response;
 };
 
 describe('User profile tests', () => {
-	it('return 400 for find one user with empty filter queries', async () => {
+	it('return 401 for request without logged in credentials', async () => {
 		await addUsers(1);
-		const findOneOption = { mobile: '09375331110' };
-		const query = {};
-		const response = await requestWithAuth(findOneOption, query);
+		const response = await getUsers(findOneURL);
+		expect(response.status).toBe(authErrorMessages.UnAuthenticated['statusCode']);
+		expect(response.body.message).toBe(authErrorMessages.UnAuthenticated['message']);
+	});
+
+	it('return 400 for find one user with empty filter queries', async () => {
+		const users = await addUsers(1);
+		const findOneOption = { mobile: users[0].mobile };
+		const filterQuery = {};
+		const response = await requestWithAuth(findOneOption, filterQuery, findOneURL);
 		expect(response.status).toBe(profileErrorMessages.EmptyFilterQuery['statusCode']);
 		expect(response.body.message).toBe(profileErrorMessages.EmptyFilterQuery['message']);
 	});
 
 	it('return 404 for find one user with wrong filter queries', async () => {
-		await addUsers(1);
-		const findOneOption = { mobile: '09375331110' };
-		const query = { firstname: 'wrong firstname' };
-		const response = await requestWithAuth(findOneOption, query);
+		const users = await addUsers(1);
+		const findOneOption = { mobile: users[0].mobile };
+		const filterQuery = { firstname: 'wrong firstname' };
+		const response = await requestWithAuth(findOneOption, filterQuery, findOneURL);
 		expect(response.status).toBe(profileErrorMessages.UserNotFound['statusCode']);
 		expect(response.body.message).toBe(profileErrorMessages.UserNotFound['message']);
 	});
 
 	it('return 200 for find one user with correct filter queries', async () => {
-		await addUsers(1);
-		const findOneOption = { mobile: '09375331110' };
-		const query = { firstname: 'user0', mobile: '09375331110' };
-		const response = await requestWithAuth(findOneOption, query);
+		const users = await addUsers(1);
+		const findOneOption = { mobile: users[0].mobile };
+		const filterQuery = { firstname: users[0].firstname, mobile: users[0].mobile };
+		const response = await requestWithAuth(findOneOption, filterQuery, findOneURL);
+		expect(response.status).toBe(200);
+	});
+
+	it('return 200 for whoami route', async () => {
+		const users = await addUsers(1);
+		const response = await requestWithAuth({}, {}, whoAmIURL);
+		expect(response.body.mobile).toBe(users[0].mobile);
+		expect(response.body.firstname).toBe(users[0].firstname);
 		expect(response.status).toBe(200);
 	});
 });
