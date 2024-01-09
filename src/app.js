25d7a515bb1069f633ae8b8f25c39a0a1294f99f
Ehsan Moallaee <ehsanm78@gmail.com>
2024-01-09 17:52:04 +0330

    Main: Cors and Joi packages installed and cors used in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import helmet from 'helmet';
@@ -15,6 +16,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(helmet());
+app.use(cors());
 app.disable('x-powered-by');
 app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
 app.use(express.urlencoded({ extended: true }));
