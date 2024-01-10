c814c59a8934768e6302c735274bd0088c1319fe
Ehsan Moallaee <ehsanm78@gmail.com>
2024-01-10 20:04:48 +0330

    Main: Roles constant added to user model and used in user model role property

--- src/modules/user/model/user.model.js
+++ src/modules/user/model/user.model.js
@@ -1,10 +1,17 @@
 import { Schema, model } from 'mongoose';
 
+export const Roles = Object.freeze({
+	SUPERADMIN: 'SUPERADMIN',
+	ADMIN: 'ADMIN',
+	USER: 'USER',
+});
+
 const userSchema = new Schema(
 	{
 		firstname: { type: String, required: true },
 		lastname: { type: String, required: true },
 		mobile: { type: String, required: true, unique: true },
+		role: { type: String, default: Roles.USER },
 	},
 	{ timestamps: true }
 );
