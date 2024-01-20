485e63753e0532159e636e69261abc28a048f35a
Ehsan Moallaee <ehsanm78@gmail.com>
2024-01-20 04:16:56 +0330

    Main: Parameter module routes improved

--- src/modules/advertise/parameterModule/parameterRoutes/v1.parameter.routes.js
+++ src/modules/advertise/parameterModule/parameterRoutes/v1.parameter.routes.js
@@ -1,47 +1,15 @@
 import { Router } from 'express';
-
-import authenticationGuard from '../../../../guards/authentication.guard.js';
-import authorizationGuard from '../../../../guards/authorization.guard.js';
 import parameterController from '../parameter.controller.js';
-import { Roles } from '../../../user/model/user.model.js';
 
 const parameterRouter_v1 = Router();
 
 // <domain>/api/v1/advertise/parameter
-parameterRouter_v1.post(
-	'/',
-	[authenticationGuard, authorizationGuard([Roles.SUPERADMIN, Roles.ADMIN])],
-	parameterController.create
-);
-parameterRouter_v1.delete(
-	'/:parameterId',
-	[authenticationGuard, authorizationGuard([Roles.SUPERADMIN, Roles.ADMIN])],
-	parameterController.delete
-);
-parameterRouter_v1.get(
-	'/by-id/:parameterId',
-	[authenticationGuard, authorizationGuard([Roles.SUPERADMIN, Roles.ADMIN])],
-	parameterController.findById
-);
-parameterRouter_v1.get(
-	'/by-category-id/:categoryId',
-	[authenticationGuard, authorizationGuard([Roles.SUPERADMIN, Roles.ADMIN])],
-	parameterController.findByCategoryId
-);
-parameterRouter_v1.get(
-	'/',
-	[authenticationGuard, authorizationGuard([Roles.SUPERADMIN, Roles.ADMIN])],
-	parameterController.fetchAll
-);
-parameterRouter_v1.patch(
-	'/:parameterId',
-	[authenticationGuard, authorizationGuard([Roles.SUPERADMIN, Roles.ADMIN])],
-	parameterController.update
-);
-parameterRouter_v1.get(
-	'/by-category-slug/:categorySlug',
-	[authenticationGuard, authorizationGuard([Roles.SUPERADMIN, Roles.ADMIN])],
-	parameterController.findByCategorySlug
-);
+parameterRouter_v1.post('/', parameterController.create);
+parameterRouter_v1.get('/by-id/:parameterId', parameterController.findById);
+parameterRouter_v1.get('/by-category-id/:categoryId', parameterController.findByCategoryId);
+parameterRouter_v1.get('/by-category-slug/:categorySlug', parameterController.findByCategorySlug);
+parameterRouter_v1.get('/', parameterController.fetchAll);
+parameterRouter_v1.patch('/:parameterId', parameterController.update);
+parameterRouter_v1.delete('/:parameterId', parameterController.delete);
 
 export default parameterRouter_v1;
--- src/modules/advertise/routes/v1.advertise.routes.js
+++ src/modules/advertise/routes/v1.advertise.routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import authenticationGuard from '../../../guards/authentication.guard.js';
+import authorizationGuard from '../../../guards/authorization.guard.js';
 import parameterRouter_v1 from '../parameterModule/parameterRoutes/v1.parameter.routes.js';
 import postRouter_v1 from '../postModule/postRoutes/v1.post.routes.js';
+import { Roles } from '../../user/model/user.model.js';
 
 const advertiseRouter_v1 = Router();
 
-advertiseRouter_v1.use('/parameter', parameterRouter_v1); // <domain>/api/v1/advertise/parameter
+advertiseRouter_v1.use(
+	'/parameter',
+	[authenticationGuard, authorizationGuard([Roles.SUPERADMIN, Roles.ADMIN])],
+	parameterRouter_v1
+); // <domain>/api/v1/advertise/parameter
 advertiseRouter_v1.use('/post', postRouter_v1); // <domain>/api/v1/advertise/post
 
 export default advertiseRouter_v1;
