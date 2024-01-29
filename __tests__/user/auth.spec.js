5d5968d11a89f110f13a16c368e0dc63641441de
Ehsan Moallaee <ehsanm78@gmail.com>
2024-01-29 20:34:58 +0330

    Main: Add tests for validation in auth service methods

--- __tests__/user/auth.spec.js
+++ __tests__/user/auth.spec.js
@@ -47,6 +47,24 @@ const incorrectShortLengthMobile = {
 	mobile: '09111',
 };
 
+const incorrectLongLengthMobile = {
+	firstname: 'firstname',
+	lastname: 'lastname',
+	mobile: '091111111111111',
+};
+
+const incorrectShortLengthFirstname = {
+	firstname: 'aa',
+	lastname: 'lastname',
+	mobile: '091111111111111',
+};
+
+const incorrectLongLengthFirstname = {
+	firstname: 'aaaaaaaaaaaaaaaaaaaaaa',
+	lastname: 'lastname',
+	mobile: '091111111111111',
+};
+
 const registerationRequestURL = '/api/v1/users/auth/registeration-request';
 const registerURL = '/api/v1/users/auth/register';
 const loginRequestURL = '/api/v1/users/auth/login-request';
@@ -60,8 +78,34 @@ describe('Authentication Register tests', () => {
 		expect(response.body.message).toBe(authSuccessMessages.OTPSentSuccessfully.message);
 	});
 
+	it('Registeration request: returns 400 with incorrect short firstname(less than 3 characters)', async () => {
+		const response = await request(app).post(registerationRequestURL).send(incorrectShortLengthFirstname);
+		expect(response.status).toBe(
+			authErrorMessages['"firstname" length must be at least 3 characters long'].statusCode
+		);
+		expect(response.body.message).toBe(
+			authErrorMessages['"firstname" length must be at least 3 characters long'].message
+		);
+	});
+
+	it('Registeration request: returns 400 with incorrect long firstname(more than 20 characters)', async () => {
+		const response = await request(app).post(registerationRequestURL).send(incorrectLongLengthFirstname);
+		expect(response.status).toBe(
+			authErrorMessages['"firstname" length must be less than or equal to 20 characters long'].statusCode
+		);
+		expect(response.body.message).toBe(
+			authErrorMessages['"firstname" length must be less than or equal to 20 characters long'].message
+		);
+	});
+
 	it('Registeration request: returns 400 with incorrect short length mobile', async () => {
 		const response = await request(app).post(registerationRequestURL).send(incorrectShortLengthMobile);
+		expect(response.status).toBe(authErrorMessages['"mobile" length must be 11 characters long'].statusCode);
+		expect(response.body.message).toBe(authErrorMessages['"mobile" length must be 11 characters long'].message);
+	});
+
+	it('Registeration request: returns 400 with incorrect long length mobile', async () => {
+		const response = await request(app).post(registerationRequestURL).send(incorrectLongLengthMobile);
 		expect(response.status).toBe(authErrorMessages['"mobile" length must be 11 characters long'].statusCode);
 		expect(response.body.message).toBe(authErrorMessages['"mobile" length must be 11 characters long'].message);
 	});
@@ -141,6 +185,26 @@ describe('Authentication Login tests', () => {
 		const response = await request(app).post(loginURL).send({ mobile: user.mobile, otpCode });
 		expect(response.status).toBe(authErrorMessages.WrongOtpCode.statusCode);
 		expect(response.body.message).toBe(authErrorMessages.WrongOtpCode.message);
+	});
+
+	it('Login (Confirm): returns 400 for request without mobile', async () => {
+		const user = await createUser(correctCredentials);
+		await request(app).post(loginRequestURL).send({ mobile: correctCredentials.mobile });
+		const data = await redisSingletonInstance.getData(user.mobile);
+		const otpCode = JSON.parse(data).otpCode;
+
+		const response = await request(app).post(loginURL).send({ otpCode });
+		expect(response.status).toBe(authErrorMessages['"mobile" is required'].statusCode);
+		expect(response.body.message).toBe(authErrorMessages['"mobile" is required'].message);
+	});
+
+	it('Login (Confirm): returns 400 for request without otp code', async () => {
+		const user = await createUser(correctCredentials);
+		await request(app).post(loginRequestURL).send({ mobile: correctCredentials.mobile });
+
+		const response = await request(app).post(loginURL).send({ mobile: user.mobile });
+		expect(response.status).toBe(authErrorMessages['"otpCode" is required'].statusCode);
+		expect(response.body.message).toBe(authErrorMessages['"otpCode" is required'].message);
 	});
 });
 
